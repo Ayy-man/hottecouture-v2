4f7cdbbd2dcd153a2a03ac0ef289032967bfde2d
Ayman Baig <ayman.beg@s.amity.edu>
2026-01-12 07:18:52 +0530

    chore: clean up troubleshooting files

--- force-sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-pgrep -i git | xargs kill -9 2>/dev/null
-rm -f .git/index.lock .git/index
-git add src/app/page.tsx
-HUSKY=0 git commit --no-verify -m "chore: force sync"
-git push origin main
